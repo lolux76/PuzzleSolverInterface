dd8ad41c806367dfec602438c2172b3444595632
Matéo Grimaud <mateogrimaud@gmail.com>
2024-01-09 09:58:54 +0100

    add home page

--- Flask/app.py
+++ Flask/app.py
@@ -1,13 +1,32 @@
-from flask import Flask, jsonify
+import os
+
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
 
 app = Flask(__name__)
 
 
-@app.route("/hello", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello from Flask"})
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/favicon.icon')
 
 
-if __name__ == "__main__":
-    # Please do not set debug=True in production
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/solve_puzzle', methods=['POST'])
+def solve_puzzle():
+    puzzle_type = request.form['puzzle_type']
+    if puzzle_type == "pasta":
+        return render_template("pasta.html")
+    return f"Vous avez choisi de résoudre le puzzle : {puzzle_type}"
+
+
+@app.route('/create_puzzle')
+def create_puzzle():
+    return "Page de création de puzzle"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
